df30d3bb4985f9445a39c3108975fb491d80a31b
reymesson1 <reymesson1>
2021-02-08 13:16:38 -0400

    done

--- confunsio7/src/App.js
+++ confunsio7/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu  from './components/MenuComponent.js';
 
 class App extends Component {
 
@@ -12,13 +13,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Dios</NavbarBrand>
           </div>
         </Navbar>
-
+        <Menu/>
       </div>
     );
   }
