15f64ebfcd6917978d879455f7231e2def75bcf8
reymesson1 <reymesson1>
2021-02-08 12:53:07 -0400

    done

--- confunsio7/src/App.js
+++ confunsio7/src/App.js
@@ -2,23 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
-import Main from './components/MainComponent';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      dishes: DISHES
-    };
   }
 
   render() {
     return (
       <div className="App">
-        <Main />
+
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Dios</NavbarBrand>
+          </div>
+        </Navbar>
+
       </div>
     );
   }
