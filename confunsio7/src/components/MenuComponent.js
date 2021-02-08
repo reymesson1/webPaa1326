6abea8d856b8a25e6b17c332ce63359de6f00efd
reymesson1 <reymesson1>
2021-02-08 17:49:36 -0400

    done

--- confunsio7/src/components/MenuComponent.js
+++ confunsio7/src/components/MenuComponent.js
@@ -8,6 +8,35 @@ class Menu extends Component{
         super(props);
 
         this.state = {
+            selectedDish: null
+        }
+    }
+
+    onDishSelect(dish){
+
+        this.setState({
+            selectedDish : dish
+        })
+    }
+
+    renderDish(dish){
+        if(dish != null){
+
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                </Card>
+            );
+        }else{
+            return(
+                <div>
+
+                </div>
+            );
         }
     }
     
@@ -20,14 +49,16 @@ class Menu extends Component{
                 return(
 
                     <div key={dish.id} className="col-12 col-md-5 m-1">
-                        <Card>
+                        <Card onClick={() => this.onDishSelect(dish)} >
                              <CardImg width="100%" src={dish.image} alt={dish.name}/>
                              <CardImgOverlay>
                                 <CardTitle>{dish.name}</CardTitle>
                             </CardImgOverlay>
                         </Card>
                     </div>
+
                 );
+
             }
         );
 
@@ -38,6 +69,9 @@ class Menu extends Component{
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
